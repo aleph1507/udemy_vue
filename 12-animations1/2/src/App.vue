325501f27ace10c1a0cb480a295219fd1241596f
aleph1507 <xrristo@gmail.com>
2018-07-23 19:57:39 +0200

    animations_proj

---
+++ 12-animations1/2/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <h1 class="text-center">The Super Quiz</h1>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Question from './components/Question.vue';
+  import Answer from './components/Answer.vue';
+
+  export default {
+    data() {
+      return {
+        mode: 'app-question'
+      }
+    },
+    methods: {
+      answered(isCorrect) {
+        if(isCorrect){
+          this.mode = 'app-answer';
+        } else {
+          this.mode = 'app-question';
+          alert('Wrong, try again');
+        }
+      }
+    },
+    components: {
+      appQuestion: Question,
+      appAnswer: Answer
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
+
+<style>
+
+</style>
