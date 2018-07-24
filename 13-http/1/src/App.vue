3281ea3dfc5bee5b8e30a93e57b9dce56013a29c
aleph1507 <xrristo@gmail.com>
2018-07-24 18:57:36 +0200

    http0000

---
+++ 13-http/1/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <h1>Http</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style lang="scss">
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
+
+<style>
+
+</style>
