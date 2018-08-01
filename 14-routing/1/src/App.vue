e582c47da58e6c8cfe820af297dd13621d7c19b0
aleph1507 <xrristo@gmail.com>
2018-08-01 21:58:08 +0200

    router0001

---
+++ 14-routing/1/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-6">
+        <h1>Routing</h1>
+        <hr>
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+
+</style>
+
+<style lang="scss">
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
---
+++ 14-routing/1/src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>The Home Page</h1>
+    <hr>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, possimus
+      delectus soluta quis magni tempore quos obcaecati eius,
+      necessitatibus esse porro laudantium atque quam sapiente,
+      et, sit laboriosam reprehenderit provident.</p>
+  </div>
+</template>
