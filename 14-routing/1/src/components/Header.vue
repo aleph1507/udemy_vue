258b7dffb6e736d813fac9b27f143a8797a8c5d1
aleph1507 <xrristo@gmail.com>
2018-08-02 19:24:03 +0200

    routing00001

---
+++ 14-routing/1/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="nav nav-pills">
+  <!-- <li class="nav-item"> -->
+    <!-- <a class="nav-link" > -->
+      <router-link class="nav-link" to="/" tag="li"
+        active-class="active" exact>
+        <a>Home</a>
+      </router-link>
+  <!-- </li> -->
+  <!-- <li class="nav-item"> -->
+    <!-- <a class="nav-link"> -->
+      <router-link class="nav-link" to="/user" tag="li"
+        active-class="active" exact>
+        <a>User</a>
+      </router-link>
+  <!-- </li> -->
+
+</ul>
+</template>
